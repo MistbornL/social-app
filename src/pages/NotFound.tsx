c1d14868bfb8d32751c567c3dbe4ca9298c66212
MistbornL <lashaloria3@gmail.com>
2023-09-22 23:57:46 +0400

    not found page fixed

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -3,14 +3,6 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <>
-      {/*
-          This example requires updating your template:
-  
-          ```
-          <html class="h-full">
-          <body class="h-full">
-          ```
-        */}
       <main className="grid min-h-full place-items-center  px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
           <p className=" font-semibold text-indigo-600 text-6xl">404</p>
