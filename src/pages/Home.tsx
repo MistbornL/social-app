f1cd580b5509a5ae057a15d3394696f85933fae0
MistbornL <lashaloria3@gmail.com>
2023-09-24 19:23:49 +0400

    fixed sizing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,18 +10,20 @@ export const Home = () => {
     <div>
       <div className="flex">
         <Sidebar />
-        <div className=" my-10 mx-auto max-w-lg  flex-col items-center">
+        <div className=" mx-auto mr-72 my-auto max-w-[935px]  flex-col items-center">
           {/* stories */}
-          <div className="mb-32">
+          <div className="mb-14">
             <h1 className="text-3xl font-bold text-white mb-5">Stories</h1>
             <SimpleSlider />
           </div>
           {/* users option to post */}
           <div></div>
           {/* all posts */}
-          <div className="mt-14 ">
-            <StatusInput />
-            <Post />
+          <div className="mt-14 flex flex-col justify-between">
+            <div className="flex flex-col ">
+              <StatusInput />
+              <Post />
+            </div>
           </div>
         </div>
       </div>
