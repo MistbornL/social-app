099eba59a590396019492566be72c38c5d9b6605
MistbornL <lashaloria3@gmail.com>
2023-10-05 15:37:23 +0400

    modal open function

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
-import Pricing from "./Pricing";
+import { Pricing } from "./Pricing";
 
 export const Layout = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -16,7 +16,7 @@ export const Layout = () => {
             className="fixed inset-0 w-screen  backdrop-blur-md z-10"
             // Close the modal when clicking the backdrop
           ></div>
-          <Pricing />
+          <Pricing setIsOpen={setIsOpen} />
         </div>
       )}{" "}
       <div className="flex lg:space-x-96">
